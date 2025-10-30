3d83118db9fcac779424d72be8eae3878564c574
drmuneb <drmuneb@gmail.com>
2025-10-30 13:53:25 +0330

    Update genkit.ts

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,14 +1,19 @@
-import {genkit} from 'genkit';
-import {googleAI} from '@genkit-ai/googleai';
-import {Config} from 'genkit';
+import {genkit, ModelReference} from 'genkit';
+import {googLeAI, GoogLeAIModel) from "@genkit-ai/googleai';
+import {z} from 'genkit';
 
 type AiConfig = {
   apiKey: string | undefined;
 };
 
 // Redefine genkit to use a dynamic config based on the provided API key.
-export const ai = (config: Config) =>
+export const ai = (config: AiConfig) =>
   genkit({
-    plugins: [googleAI({apiKey: (config as AiConfig).apiKey})],
-    model: 'googleai/gemini-2.5-flash',
+    plugins: [googleAI({apiKey: config.apiKey})],
+    models: [
+      {
+        name: 'googleai/gemini-2.5-flash',
+        path: 'gemeni-2.5-flash',
+      } as ModelReference<GoogleAIModel, z.ZodTypeAny>,
+      ],
   });
